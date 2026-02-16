4838835a163901243a266eeba37883a8320d3b14
boemalicious <91670689+boemalicious@users.noreply.github.com>
2026-02-16 12:36:33 +0100

    Update activate_env.bat

--- activate_env.bat
+++ activate_env.bat
@@ -1,2 +1,7 @@
+REM cd /d c:\python_work\project_dir
+REM .\Scripts\activate
+
+REM Change to project directory, activate environment, keep prompt
+
 cd /d c:\python_work\project_dir
-.\Scripts\activate
+cmd /k ".\Scripts\activate.bat"
